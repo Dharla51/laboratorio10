84479e5dd7b9c1a3a9b92f39246223fdb93dbfc6
Dharla51 <dharlajduran@unicesar.edu.co>
2024-10-30 18:23:29 -0500

    parte 3 lista

--- parte3.js
+++ parte3.js
@@ -1,1 +1,12 @@
-
+let esMayorDeEdad= true;  
+let tieneLicencia= true; 
+let PuedeConducir= esMayorDeEdad && tieneLicencia;  
+let PuedeConducir2=esMayorDeEdad || tieneLicencia; 
+console.log("BIENVENIDO AL CURSO DE CONDUCCION")
+console.log("PUEDE CONDUCIR: "+PuedeConducir);  
+console.log("PUEDE CONDUCIR POR SER MAYOR DE EDAD: "+esMayorDeEdad);
+document.write("BIENVENIDO AL CURSO DE CONDUCCION"); 
+document.write("<br>"); 
+document.write("PUEDE CONDUCIR: "+PuedeConducir); 
+document.write("<br>"); 
+document.write("PUEDE CONDUCIR POR SER MAYOR DE EDAD: "+esMayorDeEdad); 

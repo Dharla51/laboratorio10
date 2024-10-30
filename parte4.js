7dfe4bcf686bcdfb5f36ab14ecb62eca23aa5bcb
Dharla51 <dharlajduran@unicesar.edu.co>
2024-10-30 18:37:57 -0500

    parte 4 listo

--- parte4.js
+++ parte4.js
@@ -1,1 +1,13 @@
-
+let Nombre= prompt("DIGITE SU NOMBRE: "); 
+let Edad= prompt("DIGITE SU EDAD: ") 
+let Estudiante= prompt("¿ERES MENOR DE EDAD?(NO/SI): ").toLowerCase(); 
+if (Edad>=18){ 
+console.log("TU ERES MAYOR DE EDAD"); 
+}else{ 
+console.log("TU ERES MENOR DE EDAD"); 
+} 
+if (Estudiante=="si"){ 
+console.log("SI TIENES DERECHO A DESCUENTO POR SER ESTUDIANTE"); 
+}else { 
+    console.log("NO TIENES DERECHO A DESCUENTO POR NO SER ESTUDIANTE"); 
+    } 

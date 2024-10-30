bcbc703ce182267ca5a3b88d1c677b30ed2b7541
Dharla51 <dharlajduran@unicesar.edu.co>
2024-10-29 20:37:31 -0500

    parte1 lista

--- Parte1.js
+++ Parte1.js
@@ -1,1 +1,13 @@
 
+let Nombre= "Dharla";  
+let Edad= 18;
+let esEstudiante= true;  
+console.log("su nombre es: "+Nombre); 
+console.log("su edad es: "+ Edad); 
+console.log("es estudiante: "+esEstudiante);
+
+document.write("Hola Soy "+Nombre); 
+document.write("<br>"); 
+document.write("Mi edad es "+Edad);
+document.write("<br>");  
+document.write("Soy estudiante "+esEstudiante); 
